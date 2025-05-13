8cecfc6e66054ae2b56bdcfc1925ae710281f817
omar <omarelhamdaoui39@gmail.com>
2025-05-13 21:29:55 +0200

    update movegen

--- movegen.c
+++ movegen.c
@@ -24,26 +24,23 @@ Item* generateMoves(Piece board[8][8], char player) {
                         // Appliquer le mouvement sur la copie
                         switch (piece.type) {
                             case 'P': case 'p':
-                                success = movePawn(tempBoard, i, j, i2, j2);
-                                break;
+                                success = movePawn(tempBoard, i, j, i2, j2); break;
                             case 'R': case 'r':
-                                success = moveRook(tempBoard, i, j, i2, j2);
-                                break;
+                                success = moveRook(tempBoard, i, j, i2, j2); break;
                             case 'B': case 'b':
-                                success = moveBishop(tempBoard, i, j, i2, j2);
-                                break;
+                                success = moveBishop(tempBoard, i, j, i2, j2); break;
                             case 'Q': case 'q':
-                                success = moveQueen(tempBoard, i, j, i2, j2);
-                                break;
+                                success = moveQueen(tempBoard, i, j, i2, j2); break;
                             case 'N': case 'n':
-                                success = moveKnight(tempBoard, i, j, i2, j2);
-                                break;
+                                success = moveKnight(tempBoard, i, j, i2, j2); break;
                             case 'K': case 'k':
-                                success = moveKing(tempBoard, i, j, i2, j2);
-                                break;
+                                success = moveKing(tempBoard, i, j, i2, j2); break;
+                        }
+                        if (success) {
+                            handlePromotion(tempBoard, i2, j2, player);
                         }
 
-                        if (success) {
+                        if (success && !isInCheck(tempBoard, player)) {
                             // Créer un nouvel Item
                             Item* child = nodeAlloc();
                             memcpy(child->board, tempBoard, sizeof(tempBoard));
@@ -51,6 +48,23 @@ Item* generateMoves(Piece board[8][8], char player) {
                             child->player = (player == 'w') ? 'b' : 'w';
                             child->depth = 0;
                             child->parent = NULL;
+
+                            // Copier les indicateurs de mouvement pour le roque
+                            if (player == 'w') {
+                                child->whiteKingMoved = (i == 7 && j == 4) ? 1 : 0;
+                                child->whiteKingsideRookMoved = (i == 7 && j == 7) ? 1 : 0;
+                                child->whiteQueensideRookMoved = (i == 7 && j == 0) ? 1 : 0;
+                                child->blackKingMoved = 0;
+                                child->blackKingsideRookMoved = 0;
+                                child->blackQueensideRookMoved = 0;
+                            } else {
+                                child->blackKingMoved = (i == 0 && j == 4) ? 1 : 0;
+                                child->blackKingsideRookMoved = (i == 0 && j == 7) ? 1 : 0;
+                                child->blackQueensideRookMoved = (i == 0 && j == 0) ? 1 : 0;
+                                child->whiteKingMoved = 0;
+                                child->whiteKingsideRookMoved = 0;
+                                child->whiteQueensideRookMoved = 0;
+                            }
 
                             child->next = moveList;
                             moveList = child;
@@ -61,8 +75,11 @@ Item* generateMoves(Piece board[8][8], char player) {
         }
     }
 
+    tryCastling(board, player, &moveList);
     return moveList;
 }
+
+
 int isInCheck(Piece board[8][8], char color) {
     int king_row = -1, king_col = -1;
 
@@ -96,6 +113,151 @@ int isInCheck(Piece board[8][8], char color) {
         }
     }
 
+    return 0;
+}
+
+int tryCastling(Piece board[8][8], char player, Item** moveList) {
+    int row = (player == 'w') ? 7 : 0;
+    int king_col = 4;
+
+    if (player == 'w') {
+        // Petit roque blanc
+        if (!(*moveList)->whiteKingMoved && !(*moveList)->whiteKingsideRookMoved &&
+            board[row][5].type == ' ' && board[row][6].type == ' ' &&
+            !isInCheck(board, player)) {
+
+            Piece tempBoard[8][8];
+            memcpy(tempBoard, board, sizeof(tempBoard));
+
+            tempBoard[row][6] = tempBoard[row][4];
+            tempBoard[row][4].type = ' ';
+            tempBoard[row][4].color = ' ';
+            tempBoard[row][5] = tempBoard[row][7];
+            tempBoard[row][7].type = ' ';
+            tempBoard[row][7].color = ' ';
+
+            if (!isInCheck(tempBoard, player)) {
+                Item* child = nodeAlloc();
+                memcpy(child->board, tempBoard, sizeof(tempBoard));
+                child->player = 'b';
+                child->depth = 0;
+                child->parent = NULL;
+                child->whiteKingMoved = 1;
+                child->whiteKingsideRookMoved = 1;
+                child->next = *moveList;
+                *moveList = child;
+            }
+        }
+
+        // Grand roque blanc
+        if (!(*moveList)->whiteKingMoved && !(*moveList)->whiteQueensideRookMoved &&
+            board[row][1].type == ' ' && board[row][2].type == ' ' && board[row][3].type == ' ' &&
+            !isInCheck(board, player)) {
+
+            Piece tempBoard[8][8];
+            memcpy(tempBoard, board, sizeof(tempBoard));
+
+            tempBoard[row][2] = tempBoard[row][4];
+            tempBoard[row][4].type = ' ';
+            tempBoard[row][4].color = ' ';
+            tempBoard[row][3] = tempBoard[row][0];
+            tempBoard[row][0].type = ' ';
+            tempBoard[row][0].color = ' ';
+
+            if (!isInCheck(tempBoard, player)) {
+                Item* child = nodeAlloc();
+                memcpy(child->board, tempBoard, sizeof(tempBoard));
+                child->player = 'b';
+                child->depth = 0;
+                child->parent = NULL;
+                child->whiteKingMoved = 1;
+                child->whiteQueensideRookMoved = 1;
+                child->next = *moveList;
+                *moveList = child;
+            }
+        }
+    } else {
+        // Petit roque noir
+        if (!(*moveList)->blackKingMoved && !(*moveList)->blackKingsideRookMoved &&
+            board[row][5].type == ' ' && board[row][6].type == ' ' &&
+            !isInCheck(board, player)) {
+
+            Piece tempBoard[8][8];
+            memcpy(tempBoard, board, sizeof(tempBoard));
+
+            tempBoard[row][6] = tempBoard[row][4];
+            tempBoard[row][4].type = ' ';
+            tempBoard[row][4].color = ' ';
+            tempBoard[row][5] = tempBoard[row][7];
+            tempBoard[row][7].type = ' ';
+            tempBoard[row][7].color = ' ';
+
+            if (!isInCheck(tempBoard, player)) {
+                Item* child = nodeAlloc();
+                memcpy(child->board, tempBoard, sizeof(tempBoard));
+                child->player = 'w';
+                child->depth = 0;
+                child->parent = NULL;
+                child->blackKingMoved = 1;
+                child->blackKingsideRookMoved = 1;
+                child->next = *moveList;
+                *moveList = child;
+            }
+        }
+
+        // Grand roque noir
+        if (!(*moveList)->blackKingMoved && !(*moveList)->blackQueensideRookMoved &&
+            board[row][1].type == ' ' && board[row][2].type == ' ' && board[row][3].type == ' ' &&
+            !isInCheck(board, player)) {
+
+            Piece tempBoard[8][8];
+            memcpy(tempBoard, board, sizeof(tempBoard));
+
+            tempBoard[row][2] = tempBoard[row][4];
+            tempBoard[row][4].type = ' ';
+            tempBoard[row][4].color = ' ';
+            tempBoard[row][3] = tempBoard[row][0];
+            tempBoard[row][0].type = ' ';
+            tempBoard[row][0].color = ' ';
+
+            if (!isInCheck(tempBoard, player)) {
+                Item* child = nodeAlloc();
+                memcpy(child->board, tempBoard, sizeof(tempBoard));
+                child->player = 'w';
+                child->depth = 0;
+                child->parent = NULL;
+                child->blackKingMoved = 1;
+                child->blackQueensideRookMoved = 1;
+                child->next = *moveList;
+                *moveList = child;
+            }
+        }
+    }
+
+    return 0;
+}
+
+void handlePromotion(Piece board[8][8], int i2, int j2, char player) {
+    if (player == 'w' && i2 == 0 && board[i2][j2].type == 'P') {
+        board[i2][j2].type = 'Q';
+    } else if (player == 'b' && i2 == 7 && board[i2][j2].type == 'p') {
+        board[i2][j2].type = 'q';
+    }
+}
+
+int isCheckmate(Piece board[8][8], char player) {
+    if (!isInCheck(board, player)) return 0;
+    Item* moves = generateMoves(board, player);
+    if (moves == NULL) return 1;
+    freeList(moves);
+    return 0;
+}
+
+int isStalemate(Piece board[8][8], char player) {
+    if (isInCheck(board, player)) return 0;
+    Item* moves = generateMoves(board, player);
+    if (moves == NULL) return 1;
+    freeList(moves);
     return 0;
 }
 
--- movegen.h
+++ movegen.h
@@ -7,5 +7,11 @@
 // Fonction pour générer tous les coups possibles pour un joueur donné
 Item* generateMoves(Piece board[8][8], char player);
 int isInCheck(Piece board[8][8], char player);
+int tryCastling(Piece board[8][8], char player, Item** moveList);
+void handlePromotion(Piece board[8][8], int i2, int j2, char player);
+int isCheckmate(Piece board[8][8], char player);
+int isStalemate(Piece board[8][8], char player);
+
+
 
 #endif
