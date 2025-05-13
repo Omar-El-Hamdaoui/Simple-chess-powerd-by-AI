df5d956bb68160ad924b5c0cc4b51e55795212fa
omar <omarelhamdaoui39@gmail.com>
2025-05-13 23:53:50 +0200

    add pieces to GUI interface

--- main_gui.cpp
+++ main_gui.cpp
@@ -1,10 +1,50 @@
 #include <SFML/Graphics.hpp>
+#include <map>
+#include <string>
 
 const int SQUARE_SIZE = 75;
 const int BOARD_SIZE = 8;
 
+// Utilisation de lettres standards pour les pièces
+const char board[8][8] = {
+    {'r','n','b','q','k','b','n','r'},
+    {'p','p','p','p','p','p','p','p'},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {'P','P','P','P','P','P','P','P'},
+    {'R','N','B','Q','K','B','N','R'},
+};
+
+std::string getImageFilename(char piece) {
+    if (piece == ' ') return "";
+    std::string filename;
+    filename += (isupper(piece) ? 'w' : 'b');
+    filename += tolower(piece);
+    return "img/" + filename + ".png";
+}
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(SQUARE_SIZE * BOARD_SIZE, SQUARE_SIZE * BOARD_SIZE), "MiniChess - Echiquier");
+
+    std::map<char, sf::Texture> textures;
+    std::map<char, sf::Sprite> sprites;
+
+    // Charger les images
+    std::string allPieces = "rnbqkpRNBQKP";
+    for (char p : allPieces) {
+        std::string path = getImageFilename(p);
+        if (!textures[p].loadFromFile(path)) {
+            printf("Erreur chargement image : %s\n", path.c_str());
+            return 1;
+        }
+        sprites[p].setTexture(textures[p]);
+        sprites[p].setScale(
+            float(SQUARE_SIZE) / textures[p].getSize().x,
+            float(SQUARE_SIZE) / textures[p].getSize().y
+        );
+    }
 
     while (window.isOpen()) {
         sf::Event event;
@@ -15,19 +55,18 @@ int main() {
 
         window.clear();
 
-        // Dessiner le damier
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
                 sf::RectangleShape square(sf::Vector2f(SQUARE_SIZE, SQUARE_SIZE));
                 square.setPosition(j * SQUARE_SIZE, i * SQUARE_SIZE);
-
-                // Alterner les couleurs
-                if ((i + j) % 2 == 0)
-                    square.setFillColor(sf::Color(240, 217, 181)); // beige clair
-                else
-                    square.setFillColor(sf::Color(181, 136, 99));  // marron foncé
-
+                square.setFillColor((i + j) % 2 == 0 ? sf::Color(240, 217, 181) : sf::Color(181, 136, 99));
                 window.draw(square);
+
+                char piece = board[i][j];
+                if (piece != ' ') {
+                    sprites[piece].setPosition(j * SQUARE_SIZE, i * SQUARE_SIZE);
+                    window.draw(sprites[piece]);
+                }
             }
         }
 
